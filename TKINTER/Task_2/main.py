88763023f2a82b70ea96a19ac20b1c9ebc872e57
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-09-17 14:33:56 +0300

    Create main.py

---
+++ TKINTER/Task_2/main.py
@@ -0,0 +1,22 @@
+# pi_list = list()
+ 
+# pi1 = int(input("Введите делимое: "))
+# pi2 = int(input("Введите делитель: "))
+ 
+# for i in range(8):
+    
+#     pi3 = (pi1 / pi2)
+    
+#     pi_list.append(pi3)
+    
+#     pi2 += 2
+ 
+    
+# pi = float(pi_list[0] - pi_list[1] + pi_list[2] - pi_list[3] + pi_list[4] - pi_list[5] + pi_list[6] - pi_list[7])
+ 
+# print("Сумма ряда Лейбница первых 8 членов = ", pi)
+
+import math
+n=20
+#x=int(input("Введите искомое число"))
+#if x==1

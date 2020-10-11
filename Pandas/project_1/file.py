bac396925f46b89328bd8da1dea8687ebed9e082
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-10-11 18:44:33 +0300

    Create file.py

---
+++ Pandas/project_1/file.py
@@ -0,0 +1,23 @@
+#TASK 1
+col_list=["dep_time","dep_delay","air_time"]
+df = pd.read_csv("flights.csv", usecols=col_list)
+#saved_column = df.arr_time
+print (df)
+dp_time = df["dep_time"].between(000, 559, inclusive = True) 
+ar_time=df["air_time"].between(50,1000000, inclusive = True) 
+dp_delay=df["dep_delay"].between(-1000,60, inclusive = True) 
+print (df[dp_time][ar_time][dp_delay]) #время вылета от 0 00 до 5 59 
+# время задержки меньше 60 минут 
+# время в полете больше 50 минут !
+
+
+#TASK 2
+dest=(flights['dest'].isin(['IAH', 'HOU']))
+fligh=flights['airline'].isin(['Delta Air Lines Inc.', 'American Airlines Inc.', 'United Air Lines Inc.'])
+dates=(flights['year'] == 2013) & (flights['month'].between(6, 8)) 
+delays=(flights['dep_delay'] <= 0) & (flights['arr_delay'] > 60)
+
+print (flights[dest][fligh][dates][delays])
+
+
+#TASK 3
